c37363c29e4a7eea7a2541a38000a592512344cb
nguyenanhquoc511 <66490182+nguyenanhquoc511@users.noreply.github.com>
2023-02-24 18:37:10 +0700

    Initialize array

---
+++ Btvn1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Btvn1 {
+
+  static void printArray(int[] a) {
+    System.out.println(Arrays.toString(a));
+  }
+
+  public static void main(String[] args) {
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    printArray(a);
+
+  }
+}
+
